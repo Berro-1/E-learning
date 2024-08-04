731b3683d6a0cc2eafad090e9fcda3da4fed5b45
Berro-1 <hussienberro1@gmail.com>
2024-08-04 17:42:01 +0300

    created user model

---
+++ backend/models/User.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      validate: {
+        validator: validator.isEmail,
+        message: "Please enter a valid email.",
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["student", "admin"],
+      default: "student",
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('User',userSchema);

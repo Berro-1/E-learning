55b7063f9aff67ff56ba8a7db28ffb0da99ed47c
Berro-1 <hussienberro1@gmail.com>
2024-08-04 17:42:10 +0300

    created withdrawal model

---
+++ backend/models/Withdrawal.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const withdrawalSchema = new Schema(
+  {
+    student: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    course: { type: Schema.Types.ObjectId, ref: "Course", required: true },
+    reason: { type: String, required: true },
+    status: {
+      type: String,
+      enum: ["pending", "approved", "rejected"],
+      default: "pending",
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Withdrawal", withdrawalSchema);
